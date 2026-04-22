b2d8b5347734008ea8c924eb10be0c8ce1cdf758
Jamie L <jamie.le7@education.nsw.gov.au>
2026-04-22 10:15:01 +1000

    finished

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -4,103 +4,73 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+    private int count;
+    private int lives;
+    public float hitCooldown = 0f;
+    private float hitCooldownDuration = 1f;
+    private float movementX;
+    private float movementY;
+    public float jumpForce = 20;
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI LivesText;
+    public GameObject winTextObject;
+    private bool IsGrounded;
+    public int pity;
+    public int best;
+    public TextMeshProUGUI RewardText;
+    public string[] rewards = { "Light Cone", "Mini Herta", "FIREFLY" };
+    private bool inGatchaZone = false;
     private int gatchaCount = 0;
- private Rigidbody rb; 
 
- private int count;
-
- private int lives;
-
- public float hitCooldown = 0f;
-
- private float hitCooldownDuration = 1f;
-
- private float movementX;
- private float movementY;
-
- public float jumpForce = 20;
-
- public float speed = 0;
-
- public TextMeshProUGUI countText;
-
-public TextMeshProUGUI LivesText;
-
- public GameObject winTextObject;
-
- private bool IsGrounded;
-
- public int pity;
-
-private bool inGatchaZone = false;
-
- public int best;
-public TextMeshProUGUI RewardText;
-public string[] rewards = { "Light Cone", "Mini Herta", "FIREFLY" };
-public int luck = 0;
- void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
-
         count = 0;
-
         SetCountText();
-
         lives = 3;
-
         SetLivesText();
-
         winTextObject.SetActive(false);
-
         pity = 0;
-
         best = 0;
-
-
     }
- 
- void OnMove(InputValue movementValue)
+
+    void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
-
-        movementX = movementVector.x; 
-        movementY = movementVector.y; 
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
     public string RollGacha()
     {
-        int LightConeChance = 50 - luck;
-        int MiniHertaChance = 25 + (luck / 2);
-        int FIREFLYChance = 25 + (luck / 2);
-
         int roll = Random.Range(1, 101);
         Debug.Log("Gatcha roll: " + roll);
 
-        if (roll <= FIREFLYChance)
-            return rewards[2];
-        else if (roll <= FIREFLYChance + MiniHertaChance)
-            return rewards[1];
+        if (roll <= 5)
+            return "FIREFLY";
+        else if (roll <= 25)
+            return "Mini Herta";
         else
-            return rewards[0];
+            return "Light Cone";
     }
- private void FixedUpdate() 
+
+    private void FixedUpdate()
     {
-        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-
-        rb.AddForce(movement * speed); 
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
     }
 
- 
-void OnJump(InputValue jumpValue)
+    void OnJump(InputValue jumpValue)
     {
         if (jumpValue.isPressed && IsGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
- 
 
- void OnTriggerEnter(Collider other) 
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PickUp"))
         {
@@ -135,60 +105,7 @@ void OnJump(InputValue jumpValue)
         }
     }
 
-void OnTriggerStay(Collider other)
-{
-    // No action needed here for gatcha
-}
-
- void SetCountText() 
-    {
-
-        countText.text = "Count: " + count.ToString();
-
- if (count >= 13)
-        {
-            winTextObject.SetActive(true);
-    
-            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
-        }
-    }
-
-private void OnCollisionEnter(Collision collision)
-{
- if (collision.gameObject.CompareTag("Enemy"))
-    {
-
-            lives = lives - 1;
-
-            SetLivesText();
-            hitCooldown = hitCooldownDuration;
-    }
-    if (collision.gameObject.CompareTag("Ground"))
-        {
-             IsGrounded = true;
-        }
-    if (lives<=0)
-        {
-            Destroy(GameObject.FindGameObjectWithTag("Player"));
-            winTextObject.SetActive(true);
-             winTextObject.GetComponent<TextMeshProUGUI>().text = "you lose haha";
-        }
-
-
-}
-
-private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            IsGrounded = false;
-        }
-    }
-    void SetLivesText()
-    {
-        LivesText.text = "Lives: " + lives.ToString();
-    }
-void OnTriggerExit(Collider other)
+    void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("gatcha"))
         {
@@ -196,21 +113,68 @@ void OnTriggerExit(Collider other)
             Debug.Log("Exited gatcha zone");
         }
     }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 13)
+        {
+            winTextObject.SetActive(true);
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            lives = lives - 1;
+            SetLivesText();
+            hitCooldown = hitCooldownDuration;
+        }
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            IsGrounded = true;
+        }
+        if (lives <= 0)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("Player"));
+            winTextObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "you lose haha";
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            IsGrounded = false;
+        }
+    }
+
+    void SetLivesText()
+    {
+        LivesText.text = "Lives: " + lives.ToString();
+    }
+
     void Update()
     {
         if (hitCooldown > 0f)
             hitCooldown -= Time.deltaTime;
 
-        Debug.Log($"Update running, inGatchaZone: {inGatchaZone}");
-
         if (inGatchaZone && (
             (Keyboard.current != null && Keyboard.current.eKey.wasPressedThisFrame)
             || Input.GetKeyDown(KeyCode.E)))
         {
-            Debug.Log("E pressed in gatcha zone");
             string result = RollGacha();
             gatchaCount++;
-            RewardText.text = $"You got: {result} ({gatchaCount})";
+            RewardText.text = "You got: " + result + " (" + gatchaCount + ")";
+            Debug.Log("Result: " + result + " Count: " + gatchaCount);
+
+            if (result == "Mini Herta")
+                speed += 1;
+            else if (result == "FIREFLY")
+                speed += 50;
         }
     }
 }
